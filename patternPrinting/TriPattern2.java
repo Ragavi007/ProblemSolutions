2e4f2324e73c7a578c38edde0bf47fe7f4e1cdc4
Ragavi007 <144052788+Ragavi007@users.noreply.github.com>
2024-06-01 13:12:56 +0530

    pattern

---
+++ patternPrinting/TriPattern2.java
@@ -0,0 +1,33 @@
+package patternPrinting;
+
+/* print04
+ 
+ * 
+ * *
+ * * *
+ * * * *
+ * * *
+ * *
+ *
+ 
+ */
+
+public class TriPattern2 {
+	public static void print04(int n) {
+		for(int i =1;i<2*n;i++) {
+			int col = i>n ? 2 * n -i:i;
+			for(int j = 0;j<col;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		print04(4);
+		
+
+}
+}
