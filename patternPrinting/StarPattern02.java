8c204541cb3c8054eae66156c79f1175b292c850
Ragavi007 <144052788+Ragavi007@users.noreply.github.com>
2024-06-04 21:02:20 +0530

    fourth wd

---
+++ patternPrinting/StarPattern02.java
@@ -0,0 +1,29 @@
+package patternPrinting;
+
+public class StarPattern02 {
+	/*
+	
+    * 
+    * * 
+    * * * 
+    * * * * 
+    * * * 
+    * * 
+    * 
+	       
+	*/	
+	 public static void print(int n) {
+     	for(int i =1;i<=(2*n-1);i++) {
+     		int col = i>n ? 2*n-i:i;
+     		for(int j = 1;j<=col;j++) {
+     			System.out.print("* ");
+     		}
+     		
+     		System.out.println();
+     	}
+     	}
+	 public static void main(String[] args) {
+     	int n = 4;
+	        print(4);
+}
+}
